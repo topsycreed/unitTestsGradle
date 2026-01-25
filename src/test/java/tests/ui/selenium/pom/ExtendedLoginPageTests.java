7d05b8603631aba1264b13c69a6b861d2256837c
Gennadii Chursov <topsycreed@gmail.com>
2026-01-25 18:40:00 +0100

    Tests moved to packages

---
+++ src/test/java/tests/ui/selenium/pom/ExtendedLoginPageTests.java
@@ -0,0 +1,40 @@
+package tests.ui.selenium.pom;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.ExtendedLoginPage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Feature("Site")
+@Story("LoginPage")
+class ExtendedLoginPageTests {
+    ExtendedLoginPage login;
+
+    @BeforeEach
+    void setup() {
+        login = new ExtendedLoginPage("chrome");
+    }
+
+    @AfterEach
+    void teardown() {
+        login.quit();
+    }
+
+    @Test
+    void testLoginSuccess() {
+        login.with("user", "user");
+        assertThat(login.successBoxPresent()).isTrue();
+        assertThat(login.invalidCredentialsBoxPresent()).isFalse();
+    }
+
+    @Test
+    void testLoginFailure() {
+        login.with("test", "test");
+        assertThat(login.successBoxPresent()).isFalse();
+        assertThat(login.invalidCredentialsBoxPresent()).isTrue();
+    }
+}
